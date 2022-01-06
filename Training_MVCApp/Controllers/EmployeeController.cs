e006a0ca9ec0978eec9d12009d871fd18cc5b0ce
thevan.thavamani <thevan.thavamani@outlook.com>
2022-01-06 13:56:19 +0530

    Add project files.

---
+++ Training_MVCApp/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Training_MVCApp.Models;
+
+namespace Training_MVCApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        PersonDBEntities _dbContext;
+
+        public EmployeeController()
+        {
+            _dbContext = new PersonDBEntities();
+        }
+        public ActionResult Index()
+        {
+            ViewData["ViewDataMessage"] = "Hello World from ViewData!";
+            ViewBag.VBMessage = "Hello World from ViewBag!";
+
+            List<Person> PersonList = _dbContext.Person.ToList();
+            ViewBag.PersonLists = PersonList;
+
+            return View();
+        }
+
+        public ActionResult ShowEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ShowEmployee(MPerson ObjMPerson)
+        {
+            //Person ObjPerson = new Person
+            //{
+            //    FirstName = ObjMPerson.FirstName
+            //};
+            //_dbContext.Person.Add(ObjPerson);
+            //_dbContext.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ Training_MVCApp/Models/MPerson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Training_MVCApp.Models
+{
+    public class MPerson
+    {
+        [Required]
+        public int PersonID { get; set; }
+
+        [DisplayName("Person First Name")]
+        [Required(ErrorMessage = "Person Name is required")]
+        [StringLength(100, MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+        public Nullable<int> Age { get; set; }
+    }
+}
